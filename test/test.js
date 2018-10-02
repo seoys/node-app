48c6be32f684fa0ea74c311a1ed0191748212798
sy <sy@happytalk.io>
2018-10-02 17:34:35 +0900

    indexjs 파일 수정진행함

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
 var request = require('supertest');
-var app = require('../index.js');
 
-describe('GET /', function(){
-    it("respond with hello world", function(done) {
-
-    //navigate to root and check the the response is "hello world"
-    request(app).get("/").expect("hello world", done);
- });
-})
+describe('Test suit', function () {
+  it('should be ok', function () {
+    assert.equal(true, false);
+  });
+});
